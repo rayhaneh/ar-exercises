e33ac57cb4586a0cfb2bdb8074cc096674a4dd3f
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-29 17:22:47 +0000

    exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,3 +11,12 @@ puts "----------"
 class Employee < ActiveRecord::Base
   belongs_to :store
 end
+
+@store4 = Store.find(4)
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani",      hourly_rate: 60)
+@store1.employees.create(first_name: "John",    last_name: "Smith",       hourly_rate: 43)
+@store2.employees.create(first_name: "Alice",   last_name: "Wonderland",  hourly_rate: 30)
+@store2.employees.create(first_name: "Bob",     last_name: "Anderson",    hourly_rate: 55)
+@store2.employees.create(first_name: "Andrew",  last_name: "Peterson",    hourly_rate: 25)
+@store2.employees.create(first_name: "Peter",   last_name: "Anderson",    hourly_rate: 38)
