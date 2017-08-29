12f15567c545f13f9d01bf88cf3bb9c265b4f60c
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-08-29 16:46:52 +0000

    exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,12 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+# Create the First Store
+Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+# Print the Number of Store in the Database
+puts "----------"
+puts "Number of Store: #{Store.count}"
+puts "----------"
